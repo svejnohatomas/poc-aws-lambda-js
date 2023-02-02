4a72ebd32c2299d262bfcae0bee2b371ddf61d1c
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-02-02 00:10:22 +0000

    Update app

--- src/index.js
+++ src/index.js
@@ -1,70 +1,53 @@
+const fs = require('fs');
+const html = fs.readFileSync('index.html', { encoding: 'utf8' });
+
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
-   return 'Hello from Lambda!';
+
+   // Insert data into DynamoDB
+   if (event.queryStringParameters) {
+      await dynamo.put({
+         TableName: "pocAwsLambdaJsStore",
+         Item: {
+            faculty: event.queryStringParameters.faculty,
+            studentId: event.queryStringParameters.studentId,
+            firstName: event.queryStringParameters.firstName,
+            surname: event.queryStringParameters.surname,
+         }
+      }).promise();
+   }
+
+   // Query all data
+   let params = {
+      TableName: "pocAwsLambdaJsStore"
+   }
+   const tableQuery = await dynamo.query(params, function (err, data) {
+      if (err) console.log(err);
+      else console.log(data);
+   }).promise()
+
+   // Construct table from DynamoDB table query result
+   let modifiedHTML = dynamicTable(html, tableQuery)
+
+   const response = {
+      statusCode: 200,
+      headers: {
+         'Content-Type': 'text/html',
+      },
+      body: modifiedHTML,
+   };
+   return response;
 };
 
-
-// const fs = require('fs');
-// const html = fs.readFileSync('index.html', { encoding: 'utf8' });
-
-// const AWS = require("aws-sdk");
-// const dynamo = new AWS.DynamoDB.DocumentClient();
-
-// exports.handler = async (event) => {
-
-//    if (event.queryStringParameters) {
-//       await dynamo.put({
-//          TableName: "pocAwsLambdaJsStore",
-//          Item: {
-//             PK: "form",
-//             SK: event.requestContext.requestId,
-//             form: event.queryStringParameters
-//          }
-//       }).promise();
-//    }
-
-//    let modifiedHTML = dynamicForm(html, event.queryStringParameters);
-
-//    let params = {
-//       TableName: "pocAwsLambdaJsStore",
-//       KeyConditionExpression: "PK = :PK",
-//       ExpressionAttributeValues: {
-//          ":PK": "form"
-//       }
-//    }
-//    const tableQuery = await dynamo.query(params, function (err, data) {
-//       if (err) console.log(err);
-//       else console.log(data);
-//    }).promise()
-
-//    modifiedHTML = dynamictable(modifiedHTML, tableQuery)
-
-//    const response = {
-//       statusCode: 200,
-//       headers: {
-//          'Content-Type': 'text/html',
-//       },
-//       body: modifiedHTML,
-//    };
-//    return response;
-// };
-
-// function dynamicForm(html, queryStringParameters) {
-//    let formres = '';
-//    if (queryStringParameters) {
-//       Object.values(queryStringParameters).forEach(val => {
-//          formres = formres + val + ' ';
-//       });
-//    }
-//    return html.replace('{formResults}', '<h4>Form Submission: ' + formres + '</h4>');
-// }
-
-// function dynamictable(html, tableQuery) {
-//    let table = "";
-//    if (tableQuery.Items.length > 0) {
-//       for (let i = 0; i < tableQuery.Items.length; i++) {
-//          table = table + "<li>" + JSON.stringify(tableQuery.Items[i]) + "</li>";
-//       }
-//       table = "<pre>" + table + "</pre>";
-//    }
-//    return html.replace("{table}", "<h4>DynamoDB:</h4>" + table);
-// }
+function dynamicTable(html, tableQuery) {
+   let tbody = "";
+   if (tableQuery.Items.length > 0) {
+      for (let i = 0; i < tableQuery.Items.length; i++) {
+         let item = tableQuery.Items[i]
+         tbody += `<tr><th scope=\"row\">${item.studentId}</th><td>${item.firstName}</td><td>${item.surname}</td><td>${item.faculty}</td></tr>`
+      }
+   }
+   return html.replace("{tbody}", tbody);
+}
